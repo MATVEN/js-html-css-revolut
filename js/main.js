ebf68a4aa610c6c00673ec6935e35b935e897925
MATVEN <mattiavena2993@gmail.com>
2019-11-11 09:35:35 +0100

    setting css and js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$ (document).ready(function () {
+
+  $('.link').click(function() {
+    $('.account').toggle();
+  })
+
+  $('.link').click(function() {
+    $('.products').toggle();
+  })
+
+  $('.link').click(function() {
+    $('.company').toggle();
+  })
+
+  $('.link').click(function() {
+    $('.help').toggle();
+  })
+
+  $('.link').click(function() {
+    $('.language').toggle();
+  })
+});
